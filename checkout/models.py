551d8ed4785623e4a18775c0ed4c5441da364522
Sam Croyle <sam300385@hotmail.com>
2021-02-04 21:09:02 +0000

    Checkout app rendering, still requiring stripe and fixes

--- checkout/models.py
+++ checkout/models.py
@@ -32,19 +32,23 @@ def _generate_order_number(self):
 
 
 def update_total(self):
-    """"
-    update grand total each time a line item is added
     """
-    self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
-    self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+    self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+    if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+        self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+    else:
+        self.delivery_cost = 0
     self.grand_total = self.order_total + self.delivery_cost
     self.save()
 
 
 def save(self, *args, **kwargs):
     """
-    Override the original save method to set the order number
-    if it hasn't been set already.
+        Override the original save method to set the order number
+        if it hasn't been set already.
      """
     if not self.order_number:
         self.order_number = self._generate_order_number()
