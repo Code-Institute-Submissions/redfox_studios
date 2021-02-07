8e111bbd4a307d25474422f3cf5a005504e22587
Sam Croyle <sam300385@hotmail.com>
2021-02-07 08:31:20 +0000

    Added checkout form data to and from sending

--- redfox_studios/settings.py
+++ redfox_studios/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+from os import path
+if path.exists("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
