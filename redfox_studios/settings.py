e4db4cd318003ce82b9a802fbc2725c994263e0a
Sam Croyle <sam300385@hotmail.com>
2021-02-16 15:20:12 +0000

    Database typo in settings fixed

--- redfox_studios/settings.py
+++ redfox_studios/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = 'redfox_studios.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.getenviron.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
