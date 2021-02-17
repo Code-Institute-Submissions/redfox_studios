c8b14afcca66b6f1282ac9867a8a86a8516e14a8
Sam Croyle <sam300385@hotmail.com>
2021-02-17 21:54:40 +0000

    Code gone through validators and formatters

--- forms.py
+++ forms.py
@@ -12,4 +12,5 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
